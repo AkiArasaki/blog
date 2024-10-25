d04231182e066e56d0763b351c54e6e2c1136075
AkiArasaki-PC <tjy1367241585@gmail.com>
2024-10-25 00:04:50 -0400

    MediaBar Added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup>
-
+import MediaBar from "@/components/MediaBar.vue";
 </script>
 
 <template>
   <h1>Home</h1>
+  <div class="temp"></div>
+  <MediaBar/>
 </template>
 
 <style scoped>
-
+.temp {
+  height: 120vh;
+}
 </style>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,11 +1,17 @@
 <script setup>
 
+import MediaBar from "@/components/MediaBar.vue";
 </script>
 
 <template>
   <h1>Project</h1>
+  <div class="temp"></div>
+  <MediaBar/>
 </template>
 
 <style scoped>
-
+.temp {
+  height: 120vh;
+  background-color: #000000;
+}
 </style>
